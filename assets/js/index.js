2ffb49b658eb299649188520e0163c69a8e59aa1
mhz <954213532@qq.com>
2021-02-15 23:20:07 +0800

    user完成

--- assets/js/index.js
+++ assets/js/index.js
@@ -10,34 +10,35 @@ $(function () {
             layer.close(index);
           });
     })
-
-    function getUserInfo() {
-        $.ajax({
-            method:'GET',
-            url: "/my/userinfo",
-            // headers: {
-            //     Authorization:localStorage.getItem('token') || ''
-            // },
-            success: function (res) {
-                console.log(res);
-                if (res.status !== 0) {
-                    return layui.layer.msg('获取用户信息失败！')
-                }
-                renderAvatar(res.data)
-            }
-        });
-    }
     
-    function renderAvatar(user) {
-        var name = user.nickname || user.username
-        $('#welcome').html('欢迎&nbsp;&nbsp;'+name)
-        if (user.user_pic !== null) {
-            $('.layui-nav-img').attr('src',user.user_pic).show()
-            $('.text-avatar').hide()
-        } else {
-            $('.layui-nav-img').hide()
-            var first = name[0].toUpperCase()
-            $('.text-avatar').html(first).show()
-        }
-    }
 })
+// 要获取用户信息得是全局
+function getUserInfo() {
+    $.ajax({
+        method:'GET',
+        url: "/my/userinfo",
+        // headers: {
+        //     Authorization:localStorage.getItem('token') || ''
+        // },
+        success: function (res) {
+            console.log(res);
+            if (res.status !== 0) {
+                return layui.layer.msg('获取用户信息失败！')
+            }
+            renderAvatar(res.data)
+        }
+    });
+}
+
+function renderAvatar(user) {
+    var name = user.nickname || user.username
+    $('#welcome').html('欢迎&nbsp;&nbsp;'+name)
+    if (user.user_pic !== null) {
+        $('.layui-nav-img').attr('src',user.user_pic).show()
+        $('.text-avatar').hide()
+    } else {
+        $('.layui-nav-img').hide()
+        var first = name[0].toUpperCase()
+        $('.text-avatar').html(first).show()
+    }
+}
